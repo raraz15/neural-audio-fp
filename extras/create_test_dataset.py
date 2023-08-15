425d5dd3627ec96ee9af2fa44c6c809a27d0c119
raraz15 <oguza97@gmail.com>
2023-08-15 12:28:13 +0200

    new train, test sets test set durations 150,30 npz

--- extras/create_test_dataset.py
+++ extras/create_test_dataset.py
@@ -1,71 +1,40 @@
 import os
 import argparse
-import essentia.standard as es
+
 import numpy as np
+
+import essentia.standard as es
 
 SEED = 27
 np.random.seed(SEED)
 
-def cut_segments(audio, L, H):
-    """ Cut the audio into consecutive segments of length L with hop size H.
-    Discards the last segment."""
-
-    # Calculate the number of segments that can be cut from the audio
-    N_cut = int(np.floor((len(audio) - L + H) / H))
-    assert N_cut > 0, "audio is too short"
-    remainder = len(audio) - L - (N_cut-1)*H
-    assert remainder < L, "remainder should be smaller than L"
-
-    # Cut the signal into segments and discard the remainder
-    start_indices = np.arange(N_cut)*H
-    end_indices = start_indices + L
-    assert np.all(end_indices <= len(audio)), "end times are out of bounds"
-
-    # Get the segments
-    segments = []
-    for start,end in zip(start_indices, end_indices):
-        segment = audio[start:end]
-        segments.append([start, end, segment])
-
-    return segments
-
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Extracts segments from audio files.')
-    parser.add_argument('--query_text', 
+    parser.add_argument('query_text', 
                         type=str,
-                        default=None,
                         help='Path to the text file containing test_query audio paths.')
-    parser.add_argument('--noise_text', 
+    parser.add_argument('noise_text', 
                         type=str,
-                        default=None,
                         help='Path to the text file containing test_noise audio paths.')
-    parser.add_argument('--output_dir', 
+    parser.add_argument('output_dir', 
                         type=str,
-                        default="../test/",
-                        help='Path to the output directory. Frames will be written '
-                         'here as in single .npy file. The directory structure will be '
-                         'output_dir/test/<split>/audio_name[:2]/audio_name.npy')
-    parser.add_argument('--segment_duration', 
-                        type=float, 
-                        default=1.0,
-                        help='Duration of each segment in seconds.')
-    parser.add_argument('--hop_duration', 
-                        type=float, 
-                        default=0.5,
-                        help='Hop duration of segments in seconds.')
-    parser.add_argument('--num_segments',
-                        type=int,
-                        default=120,
-                        help='Number of segments to take from each file.')
+                        help='Path to the output directory. Segments will be written '
+                         'here as in single .npz file. The directory structure will be '
+                         'output_dir/test/<split>/audio_name[:2]/audio_name.npz')
+    parser.add_argument('--noise_chunk_duration',
+                        type=float,
+                        default=150.,
+                        help='Duration of a noise track chunk in seconds.')
+    parser.add_argument('--query_chunk_duration',
+                        type=float,
+                        default=30.,
+                        help='Duration of a query track chunk in seconds.')
     parser.add_argument("--sample_rate",
                         type=int,
                         default=8000,
                         help="Sample rate to use for audio files.")
     args = parser.parse_args()
-
-    assert args.query_text is not None or args.noise_text is not None, \
-        "At least one of --query_text or --noise_text should be provided."
 
     # Read the text files containing the audio paths
     noise_paths, query_paths = [], []
@@ -78,22 +47,21 @@ if __name__=="__main__":
             query_paths = [line.strip() for line in f.readlines()]
     print(f"Number of query files: {len(query_paths)}")
 
-    # Calculate the number of samples for each segment and hop
-    L = int(args.segment_duration * args.sample_rate)
-    H = int(args.hop_duration * args.sample_rate)
-    assert L >= H, "segment duration should be greater than or equal to hop duration"
-
     # Calculate the minimum number of samples required for each audio file
-    assert args.num_segments >= 1, "num_segments should be greater than or equal to 1"
-    min_samples = int((args.num_segments-1)*H + L)
+    assert args.noise_chunk_duration > 0, "noise_chunk_duration should be positive."
+    min_noise_samples = int(args.noise_chunk_duration * args.sample_rate)
+    assert args.query_chunk_duration > 0, "noise_chunk_duration should be positive."
+    min_query_samples = int(args.query_chunk_duration * args.sample_rate)
 
     # Cut segments from each audio file for each set and write them to disk
-    for split, paths in zip(["query_clean", "noise"], [query_paths, noise_paths]):
+    for split, paths, min_samples in zip(["query_clean", "noise"], [query_paths, noise_paths], [min_query_samples, min_noise_samples]):
 
-        # Count the number of total segments
+        # Count the number of total segments for this split
         counter = 0
 
+        # Determine the output directory for this split
         split_dir = os.path.join(args.output_dir, "test", split)
+        print(f"Writing {split} segments to {split_dir}...")
 
         if len(paths) == 0:
             print(f"No files found for {split}, skipping...")
@@ -122,29 +90,25 @@ if __name__=="__main__":
                 print(f"Audio duration is too short. Skipping {audio_path}.")
                 continue
 
-            # Get a random min_samples segment from the audio
+            # Get a random min_samples chunk from the audio
             start = np.random.randint(0, len(audio) - min_samples)
-            audio = audio[start:start+min_samples]
+            end = start + min_samples
+            chunk = audio[start:end].astype(np.float16) # Store as float16 to save space
+            boundary = np.array([start, end]).astype(np.int32)
 
-            # Cut the audio into segments
-            try:
-                segments = np.array([s[2] for s in cut_segments(audio, L, H)])
-            except AssertionError as e:
-                print(f"Segmentation failed. Skipping {audio_path}")
-                print(e)
-                continue
-
-            # Write the segments to disk as npy files
-            segments_path = os.path.join(audio_dir, audio_name+".npy")
-            with open(segments_path, "wb") as f:
-                # Write as float16 to save disk space
-                np.save(f, segments.astype(np.float16))
+            # Write the chunk to disk as a npy file
+            save_path = os.path.join(audio_dir, audio_name+".npz")
+            np.savez(save_path, 
+                     segment=chunk,
+                     boundary=boundary # Store the start and end indices of the chunk
+                     )
+            # Increment the counter for successfully segmented tracks
             counter += 1
 
             # Print progress
-            if (i+1) % 10000 == 0 or i == len(paths)-1 or i == 0:
+            if (i+1) % 10000 == 0 or (i+1) == len(paths) or i == 0:
                 print(f"{split}: [{i+1}/{len(paths)}]")
 
-        print(f"Total number of successfully segmented tracks: {counter}")
+        print(f"Total number of successfully taken chunks: {counter}")
 
     print("Done!")
--- extras/create_train_val_dataset.py
+++ extras/create_train_val_dataset.py
@@ -9,6 +9,29 @@ SEED = 27
 np.random.seed(SEED)
 
 def cut_to_segments_and_sample(audio, T_min, L0, n_segments):
+    """ First, cuts a given audio signal into n_segments non-overlapping, 
+    consecutive segments of equal length. Then samples a random sub-segment 
+    from each segment of length L0. The sub-segments are returned as a list, 
+    together with their start and end indices in the original audio signal.
+    
+        Parameters
+        ----------
+        audio : np.array
+            The audio signal to be cut and sampled.
+        T_min : float
+            The minimum duration of the audio signal in seconds.
+        L0 : int
+            The length of the sub-segments to be sampled in samples.
+        n_segments : int
+            The number of segments to cut the audio signal into.
+        
+        Returns
+        -------
+        segments : [ [start, end, segment], ... ]
+            A list of lists containing the start and end indices of the
+            sub-segments and the sub-segments themselves.
+
+    """
 
     # Check the audio duration
     T = len(audio)/SAMPLE_RATE
@@ -22,27 +45,31 @@ def cut_to_segments_and_sample(audio, T_min, L0, n_segments):
     remainder = N - N_cut
     assert remainder < L, "remainder is too large"
 
-    # Cut the signal into N_segments segments
+    # Cut the signal into N_segments non-overlapping, consecutive segments
     boundaries = np.linspace(0, N_cut, n_segments+1).astype(int)
     assert np.all(np.diff(boundaries) == L), "boundaries are not equally spaced"
 
-    # Half of the remainder goes to the first segment 
-    # the other half goes to the last segment
+    # Half of the remainder goes to the first segment and the other half goes to the last segment
     boundaries += np.floor(remainder//2).astype(int)
 
-    # Sample subsegments from each segment
+    # Sample n_segments, subsegments from each segment
     segment_offsets = np.random.randint(0, L-1-L0, size=n_segments)
     start_indices = boundaries[:-1] + segment_offsets
     end_indices = start_indices + L0
-    assert np.all(end_indices < boundaries[1:]), "end indices are out of bounds"
+    assert np.all(end_indices <= boundaries[1:]), "end indices are out of bounds"
 
-    # Get the segments
-    segments = []
+    # Get the segments and their start and end indices
+    segments, boundaries = [], []
     for start,end in zip(start_indices, end_indices):
         segment = audio[start:end]
-        segments.append([start, end, segment])
+        segments.append(segment)
+        boundaries.append([start, end])
 
-    return segments
+    # Convert to numpy arrays
+    segments = np.array(segments)
+    boundaries = np.array(boundaries)
+
+    return segments, boundaries
 
 if __name__=="__main__":
 
@@ -55,8 +82,10 @@ if __name__=="__main__":
                         help='Path to the text file containing validation audio paths.')
     parser.add_argument('output_dir', 
                         type=str,
-                        help='Path to the output directory. '
-                        'The sampled frames will be written here.')
+                        help='Path to the output directory. Sampled segments will '
+                        'be written here inside the corresponding partition. '
+                        'The directory structure will be '
+                        'output_dir/<split>/audio_name[:2]/audio_name/<segment_idx>.npz')
     parser.add_argument('--n_segments', 
                         type=int, 
                         default=59,
@@ -64,21 +93,28 @@ if __name__=="__main__":
     parser.add_argument('--segment_duration', 
                         type=float, 
                         default=2.0,
-                        help='Duration of each segment in seconds.')
+                        help='Duration of a segment in seconds.')
     args = parser.parse_args()
 
     # Minimum audio duration for no overlap
     T_min = args.segment_duration * args.n_segments
+    # Length of the sub-segments to be sampled
     L0 = np.floor(args.segment_duration * SAMPLE_RATE).astype(int)
 
     # Read the text files
     with open(args.train_text, "r") as f:
         train_paths = [line.strip() for line in f.readlines()]
+    print(f"Number of training files: {len(train_paths)}")
     with open(args.val_text, "r") as f:
         val_paths = [line.strip() for line in f.readlines()]
+    print(f"Number of validation files: {len(val_paths)}")
 
     # Sample segments from each audio file for train and val sets
     for split, paths in zip(["train", "val"], [train_paths, val_paths]):
+
+        if len(paths) == 0:
+            print(f"No files found for {split}, skipping...")
+            continue
 
         # Count the number of total segments for this split
         counter = 0
@@ -106,19 +142,21 @@ if __name__=="__main__":
                 continue
 
             try:
-                segments = cut_to_segments_and_sample(audio, T_min, L0, args.n_segments)
+                segments, boundaries = cut_to_segments_and_sample(audio, 
+                                                                T_min, 
+                                                                L0, 
+                                                                args.n_segments)
             except ValueError as e:
-                print(f"Skipping {audio_path}")
                 print(e)
+                print(f"Skipping {audio_path}.")
                 continue
 
-            # Write the segments to disk as separate files (needed for our training scheme)
-            for start,end, segment in segments:
-                segment_path = os.path.join(segments_dir, 
-                                            f"{start}_{end}.npy")
-                with open(segment_path, "wb") as f:
-                    # Write as float16 to save disk space
-                    np.save(f, segment.astype(np.float16))
+            # Write the segments to disk as separate .npz files (needed for our training scheme)
+            for j, (segment, boundary)  in enumerate(zip(segments, boundaries)):
+                np.savez(os.path.join(segments_dir, f"{j}.npz"), 
+                         segment=segment.astype(np.float16), # Write as float16 to save disk space
+                         boundary=boundary.astype(np.int32) # Write the boundaries together with the segments
+                         )
                 counter += 1
 
             # Print progress
