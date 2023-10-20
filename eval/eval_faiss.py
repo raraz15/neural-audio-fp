8aad04bd7c547f9e7b4da22255eb7acbd8a23a16
raraz15 <oguza97@gmail.com>
2023-10-20 16:00:21 +0200

    delete equally spaced

--- eval/eval_faiss.py
+++ eval/eval_faiss.py
@@ -81,13 +81,13 @@ def load_memmap_data(source_dir,
               "Numbers are separated by spaces. Default is '1 3 5 9 11 19', "
               "which corresponds to '1s, 2s, 3s, 5s, 6s, 10s' with 1 sec segment "
               "duration and 0.5 sec hop duration.")
-@click.option('--test_ids', '-t', default='equally_spaced', type=click.STRING,
-              help="One of {'all', 'equally_spaced', 'path/file.npy', (int)}. "
-              "If 'all', test all IDs from the test. You can also specify a 1-D array "
-              "file's location that contains the start indices to the the evaluation. "
-              "Any numeric input N (int) > 0 will perform search test at random position "
-              "(ID) N times. 'equally_spaced' will use boundary information to get an "
-              "equal number of samples from each track. Default is 'equally_spaced'.")
+@click.option('--test_ids', '-t', default='icassp', type=click.STRING,
+              help="One of {'all', 'icassp', 'path/file.npy', (int)}. If 'all', " +
+              "test all IDs from the test. If 'icassp', use the 2,000 " +
+              "sequence starting point IDs of 'eval/test_ids_icassp.npy' " +
+              "located in ./eval directory. You can also specify the 1-D array "
+              "file's location. Any numeric input N (int) > 0 will randomly "
+              "select N IDs. Default is 'icassp'.")
 @click.option('--k_probe', '-k', default=20, type=click.INT,
               help="Top k search for each segment. Default is 20")
 @click.option('--display_interval', '-dp', default=100, type=click.INT,
@@ -193,16 +193,6 @@ def eval_faiss(emb_dir,
         # evaluate the performance for each test_seq_len. This does not guarantee
         # getting a sample from each track.
         test_ids = np.random.permutation(len(query) - max(test_seq_len))[:int(test_ids)]
-    elif test_ids.lower() == "equally_spaced":
-        # Get an equal number of samples from each track
-        # Read the boundary of each query in the memmap
-        query_boundaries = np.load(f'{emb_dir}/query_boundaries.npy')
-        test_ids = []
-        for s,e in zip(query_boundaries[:-1], query_boundaries[1:]):
-            # Cut the query into segments of test_seq_len
-            # If the last segment is shorter than test_seq_len, ignore it
-            test_ids.append(np.arange(s,e,test_seq_len[-1])[:-1])
-        test_ids = np.concatenate(test_ids)
     elif os.path.isfile(test_ids):
         # If test_ids is a file path load it
         test_ids = np.load(test_ids)
